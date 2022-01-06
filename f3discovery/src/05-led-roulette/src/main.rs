d25d4e96b42750d2be0993267bee0d09be3dae15
alexeykarnachev <alekseykarnachev@gmail.com>
2022-01-06 18:58:18 +0300

    implement led roulette

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,21 +2,25 @@
 #![no_main]
 #![no_std]
 
-use volatile::Volatile;
 use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
     let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    let mut half_period = 3000_u16;
-    let v_half_period = Volatile::new(&mut half_period);
+    let step: u16 = 50;
+
+    let mut inds = (0..8).cycle();
+    let mut prev_idx = inds.next().unwrap();
+    leds[prev_idx].on().ok();
+    delay.delay_ms(2 * step);
 
     loop {
-        leds[0].on().ok();
-        delay.delay_ms(v_half_period.read());
-
-        leds[0].off().ok();
-        delay.delay_ms(v_half_period.read());
+        let idx = inds.next().unwrap();      
+        leds[idx].on().ok();
+        delay.delay_ms(step);
+        leds[prev_idx].off().ok();
+        prev_idx = idx;
+        delay.delay_ms(step);
     }
 }
