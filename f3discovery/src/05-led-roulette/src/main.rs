890f8c8c36025efd9ef959c8e36560a7b1b48116
alexeykarnachev <alekseykarnachev@gmail.com>
2022-01-06 19:59:54 +0300

    implement led roulette

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,14 +2,25 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let step: u16 = 50;
+
+    let mut inds = (0..8).cycle();
+    let mut prev_idx = inds.next().unwrap();
+    leds[prev_idx].on().ok();
+    delay.delay_ms(2 * step);
+
+    loop {
+        let idx = inds.next().unwrap();      
+        leds[idx].on().ok();
+        delay.delay_ms(step);
+        leds[prev_idx].off().ok();
+        prev_idx = idx;
+        delay.delay_ms(step);
+    }
 }
